f2bedd0109c7377abe81b4a47118e1f9d72dc2d6
AARAV132639 <aaravkumar2809@gmail.com>
2026-09-19 21:54:20 +0530

    updated dependencies for workmanager and room database

--- 02_To_Do_List_app/app/build.gradle.kts
+++ 02_To_Do_List_app/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.sun.tools.javac.resources.compiler
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
@@ -44,6 +46,15 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    //Room
+    implementation("androidx.room:room-runtime:2.8.5")
+    implementation("androidx.room:room-ktx:2.8.5)  " +
+            "ksp("androidx.room:room-compiler:2.8.5")
+
+    //WorkManager
+    implementation("androidx.work:work-runtime-ktx:2.11.2")
+
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
@@ -51,4 +62,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+
 }
