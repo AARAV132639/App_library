c94ff27847b26de1d58b0cc3e49e052b86dab4ce
AARAV132639 <aaravkumar2809@gmail.com>
2026-09-21 00:33:05 +0530

    updated Main activity to comprise of the changes made

--- 02_To_Do_List_app/app/src/main/java/com/example/a02_to_do_list_app/MainActivity.kt
+++ 02_To_Do_List_app/app/src/main/java/com/example/a02_to_do_list_app/MainActivity.kt
@@ -4,17 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
-
+import androidx.lifecyle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.a02_to_do_list_app.data.*
 
 class MainActivity : ComponentActivity()
 {
+    //updating the MainActivity
+    private val viewModel: TodoViewModel by viewModel{
+        val database = TodoDatabse.getDatabase(applicationContext)
+        val repository = TodoRepository(database.todoDao())
+        TodoViewModelFactory(repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContent{
-            MaterialTheme{
-                ToDoScreen()
-            }
+             ToDoScreen(viewModel = viewModel)
         }
     }
 }
