83c4c537d13ede9b1a6a80e1072176177eb79869
AARAV132639 <aaravkumar2809@gmail.com>
2026-09-21 06:56:56 +0530

    updated main activity as per current version

--- 02_To_Do_List_app/app/src/main/java/com/example/a02_to_do_list_app/MainActivity.kt
+++ 02_To_Do_List_app/app/src/main/java/com/example/a02_to_do_list_app/MainActivity.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
-import androidx.lifecyle.ViewModel
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.a02_to_do_list_app.data.*
+import androidx.activity.viewModels
+
+import com.example.a02_to_do_list_app.TodoDatabase
+import com.example.a02_to_do_list_app.TodoRepository
+import com.example.a02_to_do_list_app.TodoViewwModelFactory
 
 class MainActivity : ComponentActivity()
 {
     //updating the MainActivity
-    private val viewModel: TodoViewModel by viewModel{
-        val database = TodoDatabse.getDatabase(applicationContext)
+    private val viewModel: TodoViewModel by viewModels{
+        val database = TodoDatabase.getDatabase(applicationContext)
         val repository = TodoRepository(database.todoDao())
-        TodoViewModelFactory(repository)
+        TodoViewwModelFactory(repository)
     }
 
     override fun onCreate(savedInstanceState: Bundle?)
